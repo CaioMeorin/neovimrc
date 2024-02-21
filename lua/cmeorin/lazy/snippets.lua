36b44da03e400ca9b04228861bf501acb78ffa78
Caio Meorin <cmeorin.dev@gmail.com>
2024-02-20 22:17:50 -0300

    More Changes in lsp + snippets

--- lua/cmeorin/lazy/snippets.lua
+++ lua/cmeorin/lazy/snippets.lua
@@ -1,30 +1,19 @@
 return {
-    {
-        "L3MON4D3/LuaSnip",
-        -- follow latest release.
-        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-        -- install jsregexp (optional!).
-        build = "make install_jsregexp",
+	{
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!).
+		build = "make install_jsregexp",
 
-        dependencies = { "rafamadriz/friendly-snippets" },
+		dependencies = { "rafamadriz/friendly-snippets" },
 
-        config = function()
-            local ls = require("luasnip")
-            require("luasnip.loaders.from_vscode").lazy_load()
-            ls.filetype_extend("html", { "django" })
-            ls.filetype_extend("javascript", { "jsdoc" })
-            ls.filetype_extend("python", { "django" })
-            --- TODO: What is expand?
-            vim.keymap.set({ "i" }, "<C-s>e", function() ls.expand() end, { silent = true })
-
-            vim.keymap.set({ "i", "s" }, "<C-n>;", function() ls.jump(1) end, { silent = true })
-            vim.keymap.set({ "i", "s" }, "<C-p>,", function() ls.jump(-1) end, { silent = true })
-
-            vim.keymap.set({ "i", "s" }, "<Tab>", function()
-                if ls.choice_active() then
-                    ls.change_choice(1)
-                end
-            end, { silent = true })
-        end,
-    }
+		config = function()
+			local ls = require("luasnip")
+			require("luasnip.loaders.from_vscode").lazy_load()
+			ls.filetype_extend("html", { "django" })
+			ls.filetype_extend("javascript", { "jsdoc" })
+			ls.filetype_extend("python", { "django" })
+		end,
+	},
 }
