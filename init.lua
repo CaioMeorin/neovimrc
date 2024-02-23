054f775b2a1f3b4b70f83461f77cb24e39730eab
Caio Meorin <cmeorin.dev@gmail.com>
2024-02-23 18:56:10 -0300

    ColorColumnColors

--- init.lua
+++ init.lua
@@ -1,16 +1,16 @@
 require("cmeorin")
 
 local function MyHighlights()
-    vim.cmd("highlight ColorColumn guibg=#aa2f2f guifg=#d70000")
+	vim.cmd("highlight ColorColumn guibg=#aa2f2f guifg=#d70000")
 end
 
 vim.api.nvim_create_augroup("MyColors", {})
-vim.api.nvim_create_autocmd("ColorScheme *", {
-    group = "MyColors",
-    callback = function()
-        MyHighlights()
-    end
+vim.api.nvim_create_autocmd("ColorScheme", {
+	group = "MyColors",
+	callback = function()
+		MyHighlights()
+	end,
 })
 
-vim.cmd.colorscheme "catppuccin"
+vim.cmd.colorscheme("catppuccin")
 require("cmeorin.visualclose")
